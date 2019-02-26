32d47652367cd9ef872bf1063ecfb872e48b6fd2
Zhuikov <artem.zhuikov@mail.ru>
2019-02-26 19:21:07 +0300

    simple response. DIY page

---
+++ src/main/java/httpFtpProxy/Proxy.java
@@ -0,0 +1,48 @@
+package httpFtpProxy;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Proxy {
+
+    public static void main(String[] args) {
+
+        int port = 7500;
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Server run on port : " + port);
+
+            String line;
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Connected");
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+                while ((line = in.readLine()) != null) {
+                    System.out.println(line);
+                    if (line.isEmpty())
+                        break;
+                }
+                out.write("HTTP/1.1 200 OK\r\n");
+//                out.write("Date: Mon, 25 Feb 2019 18:12:20 GMT\r\n");
+//                out.write("Server: Apache/1.3.27\r\n");
+                out.write("Content-Type: text/html\r\n");
+                out.write("Content-Length: 57\r\n");
+                out.write("\r\n");
+                out.write("<title>Hello JAVA</title>");
+                out.write("<p>This is the best example!</p>");
+//                out.flush();
+
+                out.close();
+                in.close();
+                clientSocket.close();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
